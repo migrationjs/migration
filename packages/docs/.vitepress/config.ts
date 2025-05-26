b0788738828ef7a2fc2391cbf3eccc1fbe60a8b8
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2025-05-26 10:39:16 +0100

    Add basic docs site

---
+++ packages/docs/.vitepress/config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+	title: "Migration",
+	description: "A cross-provider Node.js database migration tool.",
+	themeConfig: {
+		// https://vitepress.dev/reference/default-theme-config
+		nav: [
+			{ text: "Home", link: "/" },
+		],
+
+		sidebar: [],
+
+		socialLinks: [
+			{
+				ariaLabel: "GitHub repository",
+				icon: "github",
+				link: "https://github.com/migrationjs/migration",
+			},
+		],
+	},
+});
---
+++ packages/docs/index.md
@@ -0,0 +1,12 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Migration"
+  text: Making migration easy
+  tagline: "A cross-provider Node.js database migration tool"
+  actions: []
+
+features: []
+---
